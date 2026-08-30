b91b3f893c8931b5e7f436bcdc750cbbc1dda85f
ardarawk-cloud <ardarawk@gmail.com>
2026-08-30 21:18:28 +0800

    Bump MOSHI alpha hotfix version

--- projects/moshi/android/app/build.gradle.kts
+++ projects/moshi/android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.ardacore.moshi"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.4.0-alpha.1"
+        versionCode = 4
+        versionName = "0.4.0-alpha.2"
     }
 
     buildFeatures {
