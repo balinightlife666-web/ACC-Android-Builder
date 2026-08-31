e88613c8e9ece25374bb07a6e0bda465035fecec
ardarawk-cloud <ardarawk@gmail.com>
2026-09-01 02:06:59 +0800

    MOSHI RC1: fix release ActivityResult dependency

--- projects/moshi/android/app/build.gradle.kts
+++ projects/moshi/android/app/build.gradle.kts
@@ -53,6 +53,7 @@ android {
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2025.02.00"))
     implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.fragment:fragment-ktx:1.8.6")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
